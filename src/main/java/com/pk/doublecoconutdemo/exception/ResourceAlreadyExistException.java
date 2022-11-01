49dc02291a39818854e15aae562cb91bc45f3780
pouyankhodabakhsh <pouyan021@gmail.com>
2022-11-02 00:19:50 +0400

    feat: custom exceptions added

---
+++ src/main/java/com/pk/doublecoconutdemo/exception/ResourceAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.pk.doublecoconutdemo.exception;
+
+public class ResourceAlreadyExistException extends Exception {
+    public ResourceAlreadyExistException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/pk/doublecoconutdemo/exception/ResourceNotExists.java
@@ -0,0 +1,7 @@
+package com.pk.doublecoconutdemo.exception;
+
+public class ResourceNotExists extends Exception {
+    public ResourceNotExists(String message) {
+        super(message);
+    }
+}
