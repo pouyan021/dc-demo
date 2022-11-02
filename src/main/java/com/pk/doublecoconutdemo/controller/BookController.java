c4aa3aa712a19d6716dc32c8e3db5a0f735deb48
pouyankhodabakhsh <pouyan021@gmail.com>
2022-11-02 14:33:56 +0400

    feat: adding getAll and improvements in BookController

--- src/main/java/com/pk/doublecoconutdemo/controller/BookController.java
+++ src/main/java/com/pk/doublecoconutdemo/controller/BookController.java
@@ -5,8 +5,11 @@ import com.pk.doublecoconutdemo.exception.ResourceNotExists;
 import com.pk.doublecoconutdemo.mapper.BookMapper;
 import com.pk.doublecoconutdemo.model.dto.request.BookCreateRequest;
 import com.pk.doublecoconutdemo.model.dto.response.BookResponse;
+import com.pk.doublecoconutdemo.model.entity.Author;
 import com.pk.doublecoconutdemo.model.entity.Book;
+import com.pk.doublecoconutdemo.service.AuthorService;
 import com.pk.doublecoconutdemo.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,31 +21,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/book/")
 public class BookController {
 
     private final BookMapper bookMapper;
     private final BookService bookService;
 
-    public BookController(BookMapper bookMapper, BookService bookService) {
-        this.bookMapper = bookMapper;
-        this.bookService = bookService;
-    }
+    private final AuthorService authorService;
 
-    @GetMapping
+
+    @GetMapping("/get")
     public ResponseEntity<BookResponse> getByTitle(@RequestParam String title) {
         Optional<Book> book = bookService.getBookByTitle(title);
         return book.map(value -> ResponseEntity.ok(bookMapper.bookToBookResponse(value)))
                 .orElseGet(() -> ResponseEntity.noContent().build());
     }
 
+    @GetMapping
+    public ResponseEntity<List<BookResponse>> getAllBooks() {
+        return ResponseEntity.ok(bookMapper.map(bookService.getAll()));
+    }
+
     @PostMapping("/create")
     public ResponseEntity<BookResponse> create(@RequestBody BookCreateRequest bookCreateRequest) throws ResourceAlreadyExistException {
+        var book = bookMapper.bookCreateRequestToBook(bookCreateRequest);
+        String authorName = bookCreateRequest.getAuthorName();
+        Optional<Author> author = authorService.getAuthorByName(authorName);
+        if (author.isPresent()) {
+           author.get().setName(authorName);
+            book.setAuthor(author.get());
+        } else {
+            Author newAuthor = new Author()
+                    .setName(authorName);
+            authorService.save(newAuthor);
+            book.setAuthor(newAuthor);
+        }
         return ResponseEntity.ok(bookMapper.bookToBookResponse(bookService
-                .save(bookMapper.bookCreateRequestToBook(bookCreateRequest))));
+                .save(book)));
     }
 
     @PutMapping("/update/{bookId}")
